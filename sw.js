7eeb8c87bd8a630726de8f39e97bf4dd75c924ff
Robert Store <robertosiolly98@gmail.com>
2026-04-02 03:26:00 -0300

    Update: Full PWA support (offline, manifest, sw), improved mobile responsiveness, and assets synchronization.

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'robert-store-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './canais.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './images/dragon_cannelone.png',
+    './images/strawberry_elephant.png',
+    './images/garama_madundung.png',
+    './images/headless_horseman.png',
+    './images/skibidi_toilet.png',
+    './images/meowl.png',
+    './images/fragama_chocrama.png',
+    './images/cerberus.png',
+    './images/la_ginger.png',
+    './images/esok_sekolah.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
